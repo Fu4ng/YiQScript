dc7d20f43cdac760056119b9b70c2e4a61b795c6
Fu4ng <903838834@qq.com>
2018-04-25 18:13:24 +0800

    php7

--- conn.php
+++ conn.php
@@ -5,10 +5,10 @@
  * Date: 2018/4/24
  * Time: 21:47
  */
-$host = '127.0.0.1';
-$name = 'root';
-$db_name = 'yiq';
-$pwd = 'toor';
-$conn = @mysqli_connect($host,$name,$db_name,$pwd) or die("error！") ;
-mysqli_query($conn,"set names utf8");
+$mysql_server_name='127.0.0.1'; //改成自己的mysql数据库服务器
+$mysql_username="root"; //改成自己的mysql数据库用户名
+$mysql_password="toor"; //改成自己的mysql数据库密码
+$mysql_database="yiq"; //改成自己的mysql数据库名
+$conn=new mysqli($mysql_server_name,$mysql_username,$mysql_password)or die("error！") ;
+$conn->query("set names ‘utf8‘");
 ?>
--- test.php
+++ test.php
@@ -18,7 +18,7 @@ else
 {
     echo "数据连接成功！";
 }
- 
+
 $conn->query("set names ‘utf8‘"); //数据库输出编码 应该与你的数据库编码保持一致.南昌网站建设公司百恒网络PHP工程师建议用UTF-8 国际标准编码.
 $conn -> select_db($mysql_database); //打开数据库
 $sql ="select * from yiq "; //SQL语句
