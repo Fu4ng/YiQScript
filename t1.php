22efd8fb80d753657f362ec5e3cf39754872c69b
Fu4ng <903838834@qq.com>
2018-04-25 20:20:41 +0800

    test

--- t1.php
+++ t1.php
@@ -21,7 +21,7 @@ else
 $conn->query("set names 'utf8'"); //数据库输出编码 应该与你的数据库编码保持一致.南昌网站建设公司百恒网络PHP工程师建议用UTF-8 国际标准编码.
 $conn -> select_db($mysql_database); //打开数据库
 $sq = "select * from USER ";
-$result = mysqli_query($conn,$sq);
+$result = mysqli_query($sq,$conn);
 $n = mysqli_num_rows($result);
 echo "查询结果有$n条记录";
 
