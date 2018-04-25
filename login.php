8bd3268cf3ea67dd5269d1bd2aad33fed2647bfb
Fu4ng <903838834@qq.com>
2018-04-25 20:45:06 +0800

    test

--- login.php
+++ login.php
@@ -12,10 +12,10 @@ $userid = $_POST['userid'];
 $userpwd = $_POST['userpwd'];
 $status = 0;
 echo "$userid";
-$sql = "select * from user where id='{$userid}'";
+$sql = "select * from user where id='1625131022'";
 $rs = mysqli_query($conn,$sql);
-if($rs['pwd'] == $userpwd){
-    $status=$rs['type'];
+if($rs["pwd"] == $userpwd){
+    $status=$rs["type"];
 }else{
        //登录失败
     $status = 0;
