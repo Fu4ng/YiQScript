f8decaf2c155b7d4ffc16b449da946c57d29dfa1
Fu4ng <903838834@qq.com>
2018-04-25 19:33:59 +0800

    test

--- t1.php
+++ t1.php
@@ -5,3 +5,11 @@
  * Date: 2018/4/25
  * Time: 19:24
  */
+
+$db=mysqlli_connect('localhost','root','toor','yiq')or die("无法连接到数据库");
+$sq = "select * from user";
+$result = mysqli_query($db,$sq);
+$n = mysqli_num_rows($result);
+echo "查询结果有".$n."条记录";
+
+?>
