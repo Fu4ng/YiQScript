6ffd548f2cf19981b4af55c1a156c2332b649b6a
Fu4ng <903838834@qq.com>
2018-04-25 19:46:56 +0800

    test

--- t1.php
+++ t1.php
@@ -5,11 +5,24 @@
  * Date: 2018/4/25
  * Time: 19:24
  */
-
-$db=mysqlli_connect('localhost','root','toor','yiq')or die("无法连接到数据库");
-//$sq = "select * from user";
-//$result = mysqli_query($db,$sq);
-//$n = mysqli_num_rows($result);
-//echo "查询结果有".$n."条记录";
+$mysql_server_name='127.0.0.1'; //改成自己的mysql数据库服务器
+$mysql_username="root"; //改成自己的mysql数据库用户名
+$mysql_password="toor"; //改成自己的mysql数据库密码
+$mysql_database="yiq"; //改成自己的mysql数据库名
+$conn=new mysqli($mysql_server_name,$mysql_username,$mysql_password); //连接数据库
+if($conn==false)
+{
+    echo "数据连接失败！";
+}
+else
+{
+    echo "数据连接成功！";
+}
+$conn->query("set names ‘utf8‘"); //数据库输出编码 应该与你的数据库编码保持一致.南昌网站建设公司百恒网络PHP工程师建议用UTF-8 国际标准编码.
+$conn -> select_db($mysql_database); //打开数据库
+$sq = "select * from USER ";
+$result = mysqli_query($conn,$sq);
+$n = mysqli_num_rows($result);
+echo "查询结果有".$n."条记录";
 
 ?>
