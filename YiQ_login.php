16b2524538cb11272e096d817235e00f6665beab
Fu4ng <903838834@qq.com>
2018-04-26 12:36:57 +0800

    ftest

--- YiQ_login.php
+++ YiQ_login.php
@@ -8,25 +8,26 @@
 
 include ("conn.php");//引用
 
-//$userid = $_POST['userid'];
-//$userpwd = $_POST['userpwd'];
-$j_objct = json_decode($_POST);
-//$sql = "select * from user where id='{$userid}'";
-//$back['status']=0;
-//$rs = mysqli_query($conn,$sql);
-//$rownum =mysqli_num_rows($rs);
-//if($rownum){
-//    $row = mysqli_fetch_array($rs);
-//    if($userpwd == $row['pwd']){
-//        //登陆成功返回身份
-//        $type = (int)$row['Type'];
-//        $back['status']=$type;
-//    }else{
-//        //登陆失败
-//        $back['status']=0;
-//    }
-//}
+$j_object = json_decode($_POST['json'],true);
+$userid = $j_object['userid'];
+$userpwd = $j_object['userpwd'];
+$sql = "select * from user where id='{$userid}'";
+$back['status']=0;
+$rs = mysqli_query($conn,$sql);
+$rownum =mysqli_num_rows($rs);
+if($rownum){
+    $row = mysqli_fetch_array($rs);
+    if($userpwd == $row['pwd']){
+        //登陆成功返回身份
+        $type = (int)$row['Type'];
+        $back['status']=$type;
+    }else{
+        //登陆失败
+        $back['status']=0;
+    }
+}
 $myfile = fopen("testfile.txt", "w");
-fwrite($myfile,$j_objct);
-//echo(json_encode($back));
+fwrite($myfile,$userid);
+fwrite($myfile,$userpwd);
+echo(json_encode($back));
 ?>
