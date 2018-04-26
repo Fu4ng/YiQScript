ab695ef8853889cc93e8c89f22e47c2ec9b7dab2
Fu4ng <903838834@qq.com>
2018-04-26 20:28:15 +0800

    f

--- get_type.php
+++ get_type.php
@@ -21,23 +21,22 @@ if($post_type == 0){
     for($i = 1;$i<=$rownum;$i++){
         $row = mysqli_fetch_assoc($rs);
         //Fid
-        $back_f[$i]['facilityid'] = $row['FID'];
+        $back_f['facilityid'] = $row['FID'];
         //设备类型
         $k = (int)$i;
         if($row['Type']==1){
-            $back_f[$k]['facilitytype']="空调";
+            $back_f['facilitytype']="空调";
         }elseif ($row['Type']==0){
-            $back_f[$k]['facilitytype']="电梯";
+            $back_f['facilitytype']="电梯";
         }
         else{
-            $back_f[$k]['facilitytype']="未知设备";
+            $back_f['facilitytype']="未知设备";
         }
         //设备地址
-        $back_f[$k]['facilityaddress']=$row['address'];
-        $back_f[$k]['facilitydetail']="无法运行";
+        $back_f['facilityaddress']=$row['address'];
+        $back_f['facilitydetail']="无法运行";
+        $back['facility'][$i] = $back_f;
     }
-    $back['facility'] =array();
-    $back['facility'] = $back_f;
 
     echo json_encode($back);
 }
