4e06e68d61e8d919373beb305444df752003f15b
Fu4ng <903838834@qq.com>
2018-04-27 12:58:59 +0800

    3

---
+++ upload_record.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fu4ng
+ * Date: 2018/4/27
+ * Time: 12:42
+ */
+include ("conn.php");
+$json_post = file_get_contents("php://input");
+$j_object = json_decode($json_post,true);
+$faclityid = $j_object['facilityid'];
+$fixadminid = $j_object['fixadminid'];
+$fixtext = $j_object['fixtext'];
+$back['fixstatus'] = 1;
+$sql = "INSERT INTO M_RECORDS (FID,ID,text) VALUE ('$faclityid','$fixadminid','$fixtext')";
+if(mysqli_query($conn,$sql)){
+    //插入成功
+    $back['fixstatus']=0;
+}else{
+    //插入失败
+    $back['fixstatus']=1;
+}
+echo json_encode($back);
+
+?>
