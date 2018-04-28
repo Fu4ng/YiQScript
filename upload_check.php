5a3a440354dcee7de70c8d183574067d25f988ae
Fu4ng <903838834@qq.com>
2018-04-28 23:52:17 +0800

    real real real save bug

--- upload_check.php
+++ upload_check.php
@@ -2,6 +2,27 @@
 /**
  * Created by PhpStorm.
  * User: Fu4ng
- * Date: 2018/4/28
- * Time: 23:41
+ * Date: 2018/4/27
+ * Time: 12:42
  */
+include ("conn.php");
+$json_post = file_get_contents("php://input");
+$j_object = json_decode($json_post,true);
+$faclityid = $j_object['facilityid'];
+$fixadminid = $j_object['fixadminid'];
+$back['fixstatus'] = 1;
+$fixsql = "UPDATE facility SET Cstatus = 1 WHERE FID = '{$faclityid}'";
+
+//修改记录
+$ur =mysqli_query($conn,$fixsql);
+if($ur){
+//插入成功
+    $back['fixstatus']=0;
+}else{
+//插入失败
+    $back['fixstatus']=1;
+}
+
+echo json_encode($back);
+
+?>
