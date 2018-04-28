7aa5a95175e9af07707de09c360addc3c21a3d15
Fu4ng <903838834@qq.com>
2018-04-28 16:50:56 +0800

    last

--- get_type.php
+++ get_type.php
@@ -20,7 +20,7 @@ if($post_type == 0){
     $rs = mysqli_query($conn,$sql);
     $rownum = mysqli_num_rows($rs);
     $back['facility']=array();
-    for($i = 0;$i<$rownum;$i++){
+    for($i = 1;$i<$rownum;$i++){
         $row = mysqli_fetch_assoc($rs);
         //Fid
         $back_f['facilityid'] = $row['FID'];
@@ -49,7 +49,7 @@ elseif ($post_type==1){
     $rs = mysqli_query($conn,$sql);
     $rownum = mysqli_num_rows($rs);
     $back['facility']=array();
-    for($i = 0;$i<=$rownum;$i++){
+    for($i = 1;$i<=$rownum;$i++){
         $row = mysqli_fetch_assoc($rs);
         //Fid
         $back_f['facilityid'] = $row['FID'];
@@ -77,7 +77,7 @@ elseif ($post_type==2){
     $rs = mysqli_query($conn,$sql);
     $rownum = mysqli_num_rows($rs);
     $back['facility']=array();
-    for($i = 0;$i<=$rownum;$i++){
+    for($i = 1;$i<=$rownum;$i++){
         $row = mysqli_fetch_assoc($rs);
         //Fid
         $back_f['facilityid'] = $row['FID'];
