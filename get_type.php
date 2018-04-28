efbc266e49ba2fd6ff7d5dedb2721cc530856be7
Fu4ng <903838834@qq.com>
2018-04-28 22:02:07 +0800

    without bug

--- get_type.php
+++ get_type.php
@@ -13,18 +13,14 @@ $json_post = file_get_contents("php://input");
 $j_object = json_decode($json_post,true);
 $post_type = $j_object['gettype'];
 $back['status'] =1;//返回的数据
-if($post_type == 3){
+if($post_type == 0){
      //待维修设备
     $back['status']=0;
-<<<<<<< HEAD
-    $sql = "select * from facility WHERE Status = 0";
-=======
-    $sql = "select * from facility WHERE Type = 1";
->>>>>>> a0171d242cf313bef5f2f384155e1e0aeb73bc03
+    $sql = "select * from facility WHERE Status =0";
     $rs = mysqli_query($conn,$sql);
     $rownum = mysqli_num_rows($rs);
     $back['facility']=array();
-    for($i = 1;$i<=$rownum;$i++){
+    for($i = 0;$i<$rownum;$i++){
         $row = mysqli_fetch_assoc($rs);
         //Fid
         $back_f['facilityid'] = $row['FID'];
@@ -40,11 +36,7 @@ if($post_type == 3){
         }
         //设备地址
         $back_f['facilityaddress']=$row['address'];
-        if($row['Status']==0){
-            $back_f['facilitydetail']="无法运行";
-        }else{
-            $back_f['facilitydetail']="正常运行";
-        }
+        $back_f['facilitydetail']="无法运行";
         array_push($back['facility'],$back_f);
     }
 
@@ -113,14 +105,14 @@ elseif ($post_type==2){
 
     echo json_encode($back);
 }
-elseif ($post_type==0){
+elseif ($post_type==3){
     //全部设备
     $back['status']=0;
     $sql = "select * from facility";
     $rs = mysqli_query($conn,$sql);
     $rownum = mysqli_num_rows($rs);
     $back['facility']=array();
-    for($i = 1;$i<$rownum;$i++){
+    for($i = 0;$i<$rownum;$i++){
         $row = mysqli_fetch_assoc($rs);
         //Fid
         $back_f['facilityid'] = $row['FID'];
